19383ee596f4f5da81f8797544a39e57b49b30e3
cklwblove <lwbhtml@163.com>
2017-11-29 13:52:37 +0800

    [build] 1.0.7

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure (y/n)" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push
+  npm publish
+fi
